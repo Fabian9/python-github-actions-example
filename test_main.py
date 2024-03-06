66b14b98692daced80d92df17166c9fcae0db6c6
Fabian Duffhauss <fabian.duffhauss@outlook.com>
2024-03-06 14:05:41 +0100

    linting errors corrected

--- test_main.py
+++ test_main.py
@@ -1,10 +1,18 @@
 from main import ret1
 
+
 def capital_case(x):
     return x.capitalize()
 
+
 def test_capital_case():
-    assert capital_case('semaphore') == 'Semaphore'
+  assert capital_case('semaphore') == 'Semaphore'
 
 def test_ret1():
     assert ret1() == 1
+
+
+
+def test_main2():
+   print("Hi")
+   
